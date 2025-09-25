122d49c5651ac183dae53ebb2b44765ae278ae55
Usman Ali <118525162+UsmanAli6t9@users.noreply.github.com>
2025-09-25 12:05:03 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,14 +60,7 @@ const App = () => {
       <ContactUs/>
       <Footer theme={theme}/>
 
-      {/* Custom cursor ring */}
-      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]' 
-      style={{transition: 'transform 0.1s ease-out'}}>
-
-      </div>
-      {/* Custom cursor dot */}
-      <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'>
-      </div>
+      
 
     </div>
   )
