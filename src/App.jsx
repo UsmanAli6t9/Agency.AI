39b678cfe09431441411209bde91f5c3b4610cfc
Usman Ali <118525162+UsmanAli6t9@users.noreply.github.com>
2025-09-25 12:06:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,43 +11,6 @@ import Footer from './components/Footer'
 
 const App = () => {
 
-  // State to manage theme (light/dark)
-  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
-
-  //refs for custom cursor elements
-  const dotRef = useRef(null)
-  const outlineRef = useRef(null)
-
-  //refs for custom cursor position tracking
-  const mouse =useRef({x:0,y:0})
-  const position =useRef({x:0,y:0})
-
-  useEffect(() =>{
-    const handleMouseMove =(e)=>{
-      mouse.current.x = e.clientX
-      mouse.current.y = e.clientY
-    }
-
-    document.addEventListener('mousemove',handleMouseMove)
-
-    const animate = () => {
-      position.current.x +=(mouse.current.x - position.current.x) * 0.1
-      position.current.y +=(mouse.current.y - position.current.y) * 0.1
-
-      if(dotRef.current && outlineRef.current){
-        dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y -6}px, 0)`
-        outlineRef.current.style.transform = `translate3d(${position.current.x - 20}px, ${position.current.y -20}px, 0)`
-      }
-
-      requestAnimationFrame(animate) 
-    }
-
-    animate()
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove)
-    }
-  },[])
 
   return (
     <div className='dark:bg-black relative'>
